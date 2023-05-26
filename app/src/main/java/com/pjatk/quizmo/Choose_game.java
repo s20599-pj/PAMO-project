9693b29f433623901de9e7fe02d0b11a591583f7
Rominski, Kamil <kamil.rominski@gmail.com>
2023-05-27 00:57:54 +0200

    Added new fragments for quiz display

---
+++ app/src/main/java/com/pjatk/quizmo/Choose_game.java
@@ -0,0 +1,54 @@
+package com.pjatk.quizmo;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pjatk.quizmo.databinding.FragmentChooseGameBinding;
+
+
+public class Choose_game extends Fragment {
+
+    private FragmentChooseGameBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentChooseGameBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.returnButtonPl2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavHostFragment.findNavController(Choose_game.this)
+                        .navigate(R.id.action_choose_game_to_menu);
+            }
+        });
+
+        binding.game1ButtonPl2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavHostFragment.findNavController(Choose_game.this)
+                        .navigate(R.id.action_choose_game_to_play);
+            }
+        });
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+}
