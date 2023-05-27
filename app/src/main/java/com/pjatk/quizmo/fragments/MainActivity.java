27f460e1bfa1185537b9414d78d152d0cfeadfa3
Rominski, Kamil <kamil.rominski@gmail.com>
2023-05-27 20:57:05 +0200

    Added missing files

--- app/src/main/java/com/pjatk/quizmo/fragments/MainActivity.java
+++ app/src/main/java/com/pjatk/quizmo/fragments/MainActivity.java
@@ -1,4 +1,4 @@
-package com.pjatk.quizmo;
+package com.pjatk.quizmo.fragments;
 
 import android.os.Bundle;
 
@@ -14,6 +14,7 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.pjatk.quizmo.R;
 import com.pjatk.quizmo.databinding.ActivityMainBinding;
 
 import android.view.Menu;
