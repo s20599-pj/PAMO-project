676f7c44594239beca070b5726c8b036de7131a1
jakub.gwiazda@simployer.com <jakub.gwiazda@simployer.com>
2023-06-12 19:30:15 +0200

    QUIZ-5 implemented leaderboard classes

--- app/src/main/java/com/pjatk/quizmo/fragments/LeaderboardManager.java
+++ app/src/main/java/com/pjatk/quizmo/fragments/LeaderboardManager.java
@@ -1,2 +1,20 @@
-package com.pjatk.quizmo.fragments;public class LeaderboardManager {
+package com.pjatk.quizmo.fragments;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeaderboardManager {
+    private List<QuizResult> quizResults;
+
+    public LeaderboardManager() {
+        quizResults = new ArrayList<>();
+    }
+
+    public void addQuizResult(QuizResult quizResult) {
+        quizResults.add(quizResult);
+    }
+
+    public List<QuizResult> getQuizResults() {
+        return quizResults;
+    }
 }
--- app/src/main/java/com/pjatk/quizmo/fragments/QuizResult.java
+++ app/src/main/java/com/pjatk/quizmo/fragments/QuizResult.java
@@ -1,2 +1,29 @@
-package com.pjatk.quizmo.fragments;public class QuizResult {
+package com.pjatk.quizmo.fragments;
+
+public class QuizResult {
+    private String playerName;
+    private int score;
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public QuizResult(String playerName, int score) {
+        this.playerName = playerName;
+        this.score = score;
+    }
+
+    // Add getters and setters for playerName and score
 }
