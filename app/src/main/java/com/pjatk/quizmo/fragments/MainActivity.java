3523920ac5f08b551ffc32e8aa5057c29f8e38e8
Rominski, Kamil <kamil.rominski@gmail.com>
2023-06-17 11:18:34 +0200

    Added support for localstorage username

--- app/src/main/java/com/pjatk/quizmo/fragments/MainActivity.java
+++ app/src/main/java/com/pjatk/quizmo/fragments/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import androidx.core.view.WindowCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
@@ -51,31 +52,9 @@ public class MainActivity extends AppCompatActivity {
         } else {
             leaderboardManager = new LeaderboardManager();
         }
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
     @Override
     public boolean onSupportNavigateUp() {
--- app/src/main/java/com/pjatk/quizmo/fragments/Menu.java
+++ app/src/main/java/com/pjatk/quizmo/fragments/Menu.java
@@ -12,6 +12,7 @@ import android.view.ViewGroup;
 
 import com.pjatk.quizmo.R;
 import com.pjatk.quizmo.databinding.FragmentMenuBinding;
+import com.pjatk.quizmo.logic.LocalStorageManager;
 import com.pjatk.quizmo.logic.QuizManager;
 import com.pjatk.quizmo.logic.QuizQuestion;
 
@@ -21,16 +22,20 @@ import java.util.List;
 public class Menu extends Fragment {
 
     private FragmentMenuBinding binding;
+    private LocalStorageManager localStorageManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentMenuBinding.inflate(inflater, container, false);
+        localStorageManager = new LocalStorageManager();
         return binding.getRoot();
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        binding.userNameField.setText(localStorageManager.getNickNameFromLocalStorage(getContext()));
 
         binding.leaderboardButtonPl2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,9 +59,18 @@ public class Menu extends Fragment {
                 args.putSerializable("quizManager", quizManager);
 
                 NavHostFragment.findNavController(Menu.this)
-                        .navigate(R.id.action_menu_to_play, args);
+                        .navigate(R.id.action_menu_to_choose_game, args);
             }
         });
+
+        binding.userNameSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newUserName = binding.userNameField.getText().toString();
+                localStorageManager.saveNickNameToLocalStorage(getContext(), newUserName);
+            }
+        });
+
     }
 
     private QuizManager createQuizManager() {
